009f064285195207be5d8c7f32b1c7eb607da788
AllaKuksa <alla.kuksa92@gmail.com>
2025-06-18 00:00:51 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 
 from database import get_db
@@ -10,8 +10,8 @@ router = APIRouter()
 
 
 @router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
-def get_movie_by_id(movie_id: int, db: Session = Depends(get_db)):
-    result = db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+async def get_movie_by_id(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
     movie = result.scalar_one_or_none()
     if not movie:
         raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
@@ -19,19 +19,22 @@ def get_movie_by_id(movie_id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/movies/", response_model=MovieListResponseSchema)
-def get_movies(
-        db: Session = Depends(get_db),
+async def get_movies(
+        db: AsyncSession = Depends(get_db),
         page: int = Query(1, ge=1),
         per_page: int = Query(10, ge=1, le=20)
 ):
-    total_items_result = db.execute(select(func.count(MovieModel.id)))
+    total_items_result = await db.execute(select(func.count(MovieModel.id)))
     total_items = total_items_result.scalar_one()
 
     total_pages = (total_items + per_page - 1) // per_page if total_items > 0 else 0
 
     offset = (page - 1) * per_page
-    result = db.execute(select(MovieModel).offset(offset).limit(per_page))
+    result = await db.execute(select(MovieModel).offset(offset).limit(per_page))
     movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
 
     return {
         "movies": movies,
